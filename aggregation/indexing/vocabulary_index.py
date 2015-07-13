3d73767fe30c310d56023ee94bb2c1a15b854e0e
Ciprian Truică <ciprian.octavian.truica@gmail.com>
2015-07-13 19:42:45 +0200

    Updated all

--- aggregation/indexing/vocabulary_index.py
+++ aggregation/indexing/vocabulary_index.py
@@ -10,6 +10,7 @@ __status__ = "Production"
 import pymongo
 from time import time
 
+
 mapFunction = """function() {
                     for (var idx=0; idx<this.words.length; idx++){
                         var key = this.words[idx].word;
